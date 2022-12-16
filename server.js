1c1b5d6f89d605946d0e3c6c98dad0d531ccfdfe
Rahul Vishwakarma <vishwakarma.rahul77@gmail.com>
2022-12-16 15:59:39 +0530

    edited cors policy

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app-rahul.vercel.app/"],
+    origin: ["http://localhost:3000", "https://pinvent-app-rahul.vercel.app"],
     credentials: true,
   })
 );
